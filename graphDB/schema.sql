0d09f48cd6e07c2ddcabb58abb9bee9488245f4d
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-28 17:30:53 +0100

    schema for db

---
+++ graphDB/schema.sql
@@ -0,0 +1,23 @@
+--Paths
+CREATE TABLE graph_paths (
+    id INTEGER PRIMARY KEY AUTOINCREMENT,
+    state INTEGER NOT NULL,
+    size INTEGER NOT NULL,
+    cars INTEGER NOT NULL
+);
+
+-- Path ends
+CREATE TABLE path_ends (
+    parent_id INTEGER PRIMARY KEY,
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL
+    FOREIGN KEY (parent_id) REFERENCES graph_paths
+    UNIQUE(parent_id, longitude, latitude)
+)
+
+--Nodes
+CREATE TABLE graph_nodes (
+    id INTEGER PRIMARY KEY UNIQUE,
+    longitude REAL NOT NULL,
+    latitude REAL NOT NULL
+)
