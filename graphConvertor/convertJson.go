7b2553f7d8db658e637d621b650809f56d1a25c7
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-28 17:06:38 +0100

    costum geometry data wrappers

--- graphConvertor/convertJson.go
+++ graphConvertor/convertJson.go
@@ -35,7 +35,7 @@ func GoOverGraph(graph node.Graph) (paths, joints gj.FeatureCollection[gj.Geomet
                 lastConn,
                 map[string]any{
                     "state": conn.State,
-                    "size": conn.State,
+                    "size": conn.Size,
                     "cars": conn.NCars})
             paths = append(paths, lastFeat)
         }
@@ -50,7 +50,7 @@ type GeoNode struct {
     Id int      `json:"id"`
 }
 
-func PointToGeoNode(point gj.Feature[gj.Geometry]) (GeoNode, error){
+func PointToGeoNode(point gj.Feature[gj.Geometry]) (GeoNode, error) {
     if point.Geometry.GeometryType != gj.PointT {
         return *new(GeoNode), errors.New("not a point")
     }
@@ -61,4 +61,56 @@ func PointToGeoNode(point gj.Feature[gj.Geometry]) (GeoNode, error){
     } else {
         return GeoNode{point.Geometry.Coords[0], id}, nil
     }
+}
+
+func (g GeoNode) ToPoint() gj.Feature[gj.Geometry] {
+    point := gj.CreatePoint(g.Coordinate)
+    return gj.WrapFeature(point, map[string]any{"id":g.Id})
+}
+
+
+type GeoPath struct {
+    Ends [2]gj.Coordinate   `json:"coordinates"`
+    State node.ConnState    `json:"state"`
+    Size int                `json:"size"`
+    Cars int                `json:"cars"`
+}
+
+func LineStringToGeoPath(line gj.Feature[gj.Geometry]) (GeoPath, error) {
+    if line.Geometry.GeometryType != gj.LineStringT {
+        return *new(GeoPath), errors.New("line")
+    }
+    if len(line.Geometry.Coords) != 2 {
+        return *new(GeoPath), errors.New("doesnt have only two ends")
+    }
+
+    var (
+        state node.ConnState
+        size, cars int
+    )
+
+    if val, ok := line.Props["state"]; !ok {
+        return *new(GeoPath), errors.New("missing state")
+    } else if state, ok = val.(node.ConnState); !ok {
+        return *new(GeoPath), errors.New("not a state")
+    }
+
+    if val, ok := line.Props["size"]; !ok {
+        return *new(GeoPath), errors.New("missing size")
+    } else if size, ok = val.(int); !ok {
+        return *new(GeoPath), errors.New("not a size")
+    }
+
+    if val, ok := line.Props["cars"]; !ok {
+        return *new(GeoPath), errors.New("missing cars")
+    } else if cars, ok = val.(int); !ok {
+        return *new(GeoPath), errors.New("not a cars")
+    }
+
+    return GeoPath{[2]gj.Coordinate(line.Geometry.Coords), state, size, cars}, nil
+}
+
+func (g GeoPath) ToLineString() gj.Feature[gj.Geometry] {
+    line, _ := gj.CreateLineString(g.Ends[0], g.Ends[1])
+    return gj.WrapFeature(line, map[string]any{"state": g.State, "size": g.Size, "cars": g.Cars})
 }
