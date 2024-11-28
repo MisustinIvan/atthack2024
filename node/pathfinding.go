48edf80a64ae102f451914b2f545d09ac84a114b
Ivan Misustin <ivanmis596@gmail.com>
2024-11-28 16:43:42 +0100

    pathfinding dijkstra done

---
+++ node/pathfinding.go
@@ -0,0 +1,72 @@
+package node
+
+import (
+	"container/heap"
+	"errors"
+	"math"
+	"optitraffic/pqueue"
+)
+
+type Pathfinder struct {
+	graph *Graph
+}
+
+func NewPathfinder(g *Graph) Pathfinder {
+	return Pathfinder{
+		graph: g,
+	}
+}
+
+func (pf *Pathfinder) Path(from, to *Node) ([]*Node, error) {
+	// Edge case: if the from and to nodes are the same
+	if from == to {
+		return []*Node{from}, nil
+	}
+
+	// Initialize distances and the priority queue
+	distances := make(map[*Node]float64)
+	cameFrom := make(map[*Node]*Node)
+	for _, node := range pf.graph.Nodes {
+		distances[node] = math.MaxFloat64
+	}
+	distances[from] = 0
+
+	// Create a priority queue and add the starting node
+	pq := make(pqueue.PriorityQueue[*Node], 0)
+	heap.Init(&pq)
+	heap.Push(&pq, pqueue.NewItem(from, 0, 0))
+
+	// Dijkstra's algorithm
+	for pq.Len() > 0 {
+		currentItem := heap.Pop(&pq).(*pqueue.Item[*Node])
+		currentNode := currentItem.Value()
+
+		// If we reached the target node, stop processing
+		if currentNode == to {
+			break
+		}
+
+		// Relaxation: Update distances to neighbors
+		for neighbor, params := range currentNode.Conns {
+			newDist := distances[currentNode] + params.ConnectionWeight()
+			if newDist < distances[neighbor] {
+				distances[neighbor] = newDist
+				cameFrom[neighbor] = currentNode
+				heap.Push(&pq, pqueue.NewItem(neighbor, int(newDist), neighbor.Id))
+			}
+		}
+	}
+
+	// Reconstruct the path
+	path := []*Node{}
+	for current := to; current != nil; current = cameFrom[current] {
+		path = append([]*Node{current}, path...)
+	}
+
+	// If we couldn't reach the destination node, return an error
+	if len(path) == 0 || path[0] != from {
+		return nil, errors.New("no path found")
+	}
+
+	return path, nil
+}
---
+++ node/pathfinding_test.go
@@ -0,0 +1,38 @@
+package node_test
+
+import (
+	"fmt"
+	"optitraffic/node"
+	"testing"
+)
+
+func TestPathfinding(t *testing.T) {
+	graph := node.NewGraph(node.Pos{0, 0})
+	root := graph.Root
+	node1 := graph.NewNode(node.Pos{-1, 1})
+	node2 := graph.NewNode(node.Pos{-1, 2})
+	node3 := graph.NewNode(node.Pos{1, 2})
+	node4 := graph.NewNode(node.Pos{1, 1})
+	node5 := graph.NewNode(node.Pos{2, 2})
+	node6 := graph.NewNode(node.Pos{0, 3})
+
+	graph.LinkBoth(root, node1, 1)
+	graph.LinkBoth(root, node4, 1)
+	graph.LinkBoth(node1, node4, 1)
+	graph.LinkBoth(node1, node6, 1)
+	graph.LinkBoth(node1, node2, 1)
+	graph.LinkBoth(node4, node5, 1)
+	graph.LinkBoth(node4, node3, 1)
+	graph.LinkBoth(node5, node6, 1)
+	graph.LinkBoth(node3, node6, 1)
+	graph.LinkBoth(node3, node2, 1)
+
+	pf := node.NewPathfinder(&graph)
+	_, err := pf.Path(root, node6)
+	if err != nil {
+		for _, node := range graph.Nodes {
+			fmt.Printf("%v\n", *node)
+		}
+		t.Fatal(err)
+	}
+}
