6f1ba9b5e02ebf02fab27059cb3089943a9c4897
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-29 02:19:35 +0100

    duped paths (both ways now)

--- graph_editor/main.go
+++ graph_editor/main.go
@@ -18,89 +18,97 @@ import (
 var creating_graph = false
 
 func geojson_string_pair_parse(input string) [2]string {
-	// Split the input string at the boundary between two FeatureCollections
-	parts := strings.Split(input, `},{"type":"FeatureCollection"`)
+    // Split the input string at the boundary between two FeatureCollections
+    parts := strings.Split(input, `},{"type":"FeatureCollection"`)
 
-	// Add back the missing curly braces around the two parts
-	part1 := parts[0] + `}`
-	part1 = part1[1:]
+    // Add back the missing curly braces around the two parts
+    part1 := parts[0] + `}`
+    part1 = part1[1:]
 
-	part2 := `{"type":"FeatureCollection"` + parts[1]
-	part2 = part2[:len(part2)-1]
+    part2 := `{"type":"FeatureCollection"` + parts[1]
+    part2 = part2[:len(part2)-1]
 
-	// Create an array with the two strings
-	return [2]string{part1, part2}
+    // Create an array with the two strings
+    return [2]string{part1, part2}
 }
 
 func main() {
-	db, err := sql.Open("sqlite3", "../graphDB/db.db")
-	if err != nil {
-		log.Fatal(err)
-	}
-	dao := graphdb.NewDAO(db)
+    db, err := sql.Open("sqlite3", "../graphDB/db.db")
+    if err != nil {
+        log.Fatal(err)
+    }
+    dao := graphdb.NewDAO(db)
 
-	app := fiber.New()
+    app := fiber.New()
 
-	templates, err := templates.NewTemplates()
-	if err != nil {
-		log.Fatal(err)
-	}
+    templates, err := templates.NewTemplates()
+    if err != nil {
+        log.Fatal(err)
+    }
 
-	app.Get("/index.js", func(c *fiber.Ctx) error {
-		return c.SendFile("./index.js")
-	})
+    app.Get("/index.js", func(c *fiber.Ctx) error {
+        return c.SendFile("./index.js")
+    })
 
-	app.Get("/", func(c *fiber.Ctx) error {
-		return templates.Render("index", nil, c)
-	})
+    app.Get("/", func(c *fiber.Ctx) error {
+        return templates.Render("index", nil, c)
+    })
 
-	app.Get("/creator_not_creating", func(c *fiber.Ctx) error {
-		return templates.Render("creator_not_creating", nil, c)
-	})
+    app.Get("/creator_not_creating", func(c *fiber.Ctx) error {
+        return templates.Render("creator_not_creating", nil, c)
+    })
 
-	app.Get("/create_graph", func(c *fiber.Ctx) error {
-		return templates.Render("creator_creating", nil, c)
-	})
+    app.Get("/create_graph", func(c *fiber.Ctx) error {
+        return templates.Render("creator_creating", nil, c)
+    })
 
-	app.Get("/save_graph", func(c *fiber.Ctx) error {
-		return templates.Render("creator_not_creating", nil, c)
-	})
+    app.Get("/save_graph", func(c *fiber.Ctx) error {
+        return templates.Render("creator_not_creating", nil, c)
+    })
 
-	app.Post("/save_geojson", func(c *fiber.Ctx) error {
-		parts := geojson_string_pair_parse(string(c.Body()))
+    app.Post("/save_geojson", func(c *fiber.Ctx) error {
+        parts := geojson_string_pair_parse(string(c.Body()))
 
-		resp_points, err := geojson.FeatureCollFromJSON[geojson.FlatGeometry](parts[0])
-		if err != nil {
-			return err
-		}
+        resp_points, err := geojson.FeatureCollFromJSON[geojson.FlatGeometry](parts[0])
+        if err != nil {
+            return err
+        }
 
-		resp_lines, err := geojson.FeatureCollFromJSON[geojson.Geometry](parts[1])
-		if err != nil {
-			return err
-		}
+        resp_lines, err := geojson.FeatureCollFromJSON[geojson.Geometry](parts[1])
+        if err != nil {
+            return err
+        }
 
-		resp_nodes, err := graphconvertor.PointsCollToGeoNode(resp_points)
-		if err != nil {
-			return err
-		}
-		err = dao.StoreGeoNodes(resp_nodes...)
-		if err != nil {
-			fmt.Printf("shit")
-			return err
-		}
+        resp_nodes, err := graphconvertor.PointsCollToGeoNode(resp_points)
+        if err != nil {
+            return err
+        }
+        err = dao.StoreGeoNodes(resp_nodes...)
+        if err != nil {
+            fmt.Printf("shit")
+            return err
+        }
 
-		resp_paths, err := graphconvertor.LineCollToGeoPath(resp_lines)
-		if err != nil {
-			return err
-		}
-		err = dao.StoreGeoPaths(resp_paths...)
-		if err != nil {
-			fmt.Printf("fuck")
-			return err
-		}
+        resp_paths, err := graphconvertor.LineCollToGeoPath(resp_lines)
+        if err != nil {
+            return err
+        }
+        err = dao.StoreGeoPaths(DupePaths(resp_paths...)...)
+        if err != nil {
+            fmt.Printf("fuck")
+            return err
+        }
 
-		return c.SendStatus(fiber.StatusOK)
-	})
+        return c.SendStatus(fiber.StatusOK)
+    })
 
-	app.Listen(":6969")
+    app.Listen(":6969")
+}
+
+func DupePaths(lines ...graphconvertor.GeoPath) []graphconvertor.GeoPath {
+    out := make([]graphconvertor.GeoPath, 0, len(lines)*2)
+    for _, v := range lines {
+        out = append(out, v, graphconvertor.GeoPath{Ends: [2]geojson.Coordinate{v.Ends[1], v.Ends[0]}, State: v.State, Size: v.Size, Cars: v.Cars})
+    }
+    return out
 }
