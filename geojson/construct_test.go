686b4d2266b7d029f27fac7eae28728f125e22f8
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-29 01:29:53 +0100

    test fix

--- geojson/construct_test.go
+++ geojson/construct_test.go
@@ -4,42 +4,42 @@ import (
 	"testing"
 )
 
-// func TestCreateColl(t *testing.T) {
-//     points := []FlatGeometry{
-//         CreatePoint(CreateCoordinate(0, 50)),
-//         CreatePoint(CreateCoordinate(1, 50)),
-//         CreatePoint(CreateCoordinate(1, 51)),
-//     }
-//     geoColl := CreateGeometryColl(points...)
-//     json, err := geoColl.ToJSON()
-//     if err != nil {
-//         t.Fail()
-//     }
-//     print(json)
-// }
+func TestCreateColl(t *testing.T) {
+    points := []FlatGeometry{
+        CreatePoint(CreateCoordinate(0, 50)),
+        CreatePoint(CreateCoordinate(1, 50)),
+        CreatePoint(CreateCoordinate(1, 51)),
+    }
+    geoColl := CreateGeometryColl(points...)
+    json, err := geoColl.ToJSON()
+    if err != nil {
+        t.Fail()
+    }
+    print(json)
+}
 
-// func TestFeature(t *testing.T) {
-//     point := CreatePoint(CreateCoordinate(0, 50))
-//     feature := Feature[FlatGeometry]{point, nil}
-//     json, err := feature.ToJSON()
-//     if err != nil {
-//         t.Fail()
-//     }
-//     print(json)
-//     callback, err := FeatureFromJSON[Geometry](json)
-//     if err != nil {
-//         t.Fail()
-//     }
-//     if !areEqualFeature(feature, callback) {
-//         t.Errorf("result not equal")
-//     }
-// }
+func TestFeature(t *testing.T) {
+    point := CreatePoint(CreateCoordinate(0, 50))
+    feature := Feature[FlatGeometry]{point, nil}
+    json, err := feature.ToJSON()
+    if err != nil {
+        t.Fail()
+    }
+    print(json)
+    callback, err := FeatureFromJSON[FlatGeometry](json)
+    if err != nil {
+        t.Fail()
+    }
+    if !areEqualFeature(feature, callback) {
+        t.Errorf("result not equal")
+    }
+}
 
 func Test(t *testing.T) {
 
 }
 
-func areEqualFeature(a, b Feature[Geometry]) bool {
+func areEqualFeature(a, b Feature[FlatGeometry]) bool {
     aNil, bNil := a.Props == nil, b.Props == nil
     if aNil && !bNil {
         return false
@@ -54,8 +54,8 @@ func areEqualFeature(a, b Feature[Geometry]) bool {
     if a.Geometry.GeometryType != b.Geometry.GeometryType {
         return false
     }
-    for i, v := range a.Geometry.Coords {
-        if b.Geometry.Coords[i] != v {
+    for i, v := range a.Geometry.SingleCoords {
+        if b.Geometry.SingleCoords[i] != v {
             return false
         }
     }
