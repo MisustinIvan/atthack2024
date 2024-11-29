e4384c90f6d04c2daf3d792a0518154cff4afa69
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-29 04:34:44 +0100

    cooked with pathfinding

--- node/pathfinding.go
+++ node/pathfinding.go
@@ -8,88 +8,105 @@ import (
 	"optitraffic/pqueue"
 )
 
+type VehicleType int
+
+const (
+    NormalVehicle VehicleType = iota
+    EmergencyVehicle
+)
+
 type Pathfinder struct {
-	graph *Graph
+    graph *Graph
 }
 
 func NewPathfinder(g *Graph) Pathfinder {
-	return Pathfinder{
-		graph: g,
-	}
+    return Pathfinder{
+        graph: g,
+    }
 }
 
-func (pf *Pathfinder) Path(from, to *Node) ([]*Node, error) {
-	// Edge case: if the from and to nodes are the same
-	if from == to {
-		return []*Node{from}, nil
-	}
+func (pf *Pathfinder) Path(from, to *Node, typ VehicleType) ([]*Node, error) {
+    // Edge case: if the from and to nodes are the same
+    if from == to {
+        return []*Node{from}, nil
+    }
 
-	// Initialize distances and the priority queue
-	distances := make(map[*Node]float64)
-	cameFrom := make(map[*Node]*Node)
-	for _, node := range pf.graph.Nodes {
-		distances[node] = math.MaxFloat64
-	}
-	distances[from] = 0
+    // Initialize distances and the priority queue
+    distances := make(map[*Node]float64)
+    cameFrom := make(map[*Node]*Node)
+    for _, node := range pf.graph.Nodes {
+        distances[node] = math.MaxFloat64
+    }
+    distances[from] = 0
 
-	// Create a priority queue and add the starting node
-	pq := make(pqueue.PriorityQueue[*Node], 0)
-	heap.Init(&pq)
-	heap.Push(&pq, pqueue.NewItem(from, 0, 0))
+    // Create a priority queue and add the starting node
+    pq := make(pqueue.PriorityQueue[*Node], 0)
+    heap.Init(&pq)
+    heap.Push(&pq, pqueue.NewItem(from, 0, 0))
 
-	// Dijkstra's algorithm
-	for pq.Len() > 0 {
-		currentItem := heap.Pop(&pq).(*pqueue.Item[*Node])
-		currentNode := currentItem.Value()
+    // Dijkstra's algorithm
+    for pq.Len() > 0 {
+        currentItem := heap.Pop(&pq).(*pqueue.Item[*Node])
+        currentNode := currentItem.Value()
 
-		// If we reached the target node, stop processing
-		if currentNode == to {
-			break
-		}
+        // If we reached the target node, stop processing
+        if currentNode == to {
+            break
+        }
 
-		// Relaxation: Update distances to neighbors
-		for neighbor, params := range currentNode.Conns {
-			newDist := distances[currentNode] + params.ConnectionWeight()
-			if newDist < distances[neighbor] {
-				distances[neighbor] = newDist
-				cameFrom[neighbor] = currentNode
-				heap.Push(&pq, pqueue.NewItem(neighbor, int(newDist), neighbor.Id))
-			}
-		}
-	}
+        // Relaxation: Update distances to neighbors
+        for neighbor, params := range currentNode.Conns {
+            newDist := distances[currentNode] + params.ConnectionWeight(typ)
+            if newDist < distances[neighbor] {
+                distances[neighbor] = newDist
+                cameFrom[neighbor] = currentNode
+                heap.Push(&pq, pqueue.NewItem(neighbor, int(newDist), neighbor.Id))
+            }
+        }
+    }
 
-	// Reconstruct the path
-	path := []*Node{}
-	for current := to; current != nil; current = cameFrom[current] {
-		path = append([]*Node{current}, path...)
-	}
+    // Reconstruct the path
+    path := []*Node{}
+    for current := to; current != nil; current = cameFrom[current] {
+        path = append([]*Node{current}, path...)
+    }
 
-	// If we couldn't reach the destination node, return an error
-	if len(path) == 0 || path[0] != from {
-		return nil, errors.New("no path found")
-	}
+    // If we couldn't reach the destination node, return an error
+    if len(path) == 0 || path[0] != from {
+        return nil, errors.New("no path found")
+    }
 
-	return path, nil
+    return path, nil
 }
 
 func PathLength(nodes []*Node) float64 {
-	res := 0.0
+    res := 0.0
 
-	for n1, n2 := range IntoPairIter(nodes) {
-		res += n1.Conns[n2].Dist
-	}
+    for n1, n2 := range IntoPairIter(nodes) {
+        res += n1.Conns[n2].Dist
+    }
 
-	return res
+    return res
+}
+
+func PathWeightedLength(nodes []*Node, typ VehicleType) float64 {
+    wieghted := 0.0
+
+    for n1, n2 := range IntoPairIter(nodes) {
+        wieghted += n1.Conns[n2].ConnectionWeight(typ)
+    }
+
+    return wieghted
 }
 
 func IntoPairIter[T any](xs []T) iter.Seq2[T, T] {
-	return func(yield func(T, T) bool) {
-		for i := 0; i < len(xs)-1; i++ {
-			a := xs[i]
-			b := xs[i+1]
-			if !yield(a, b) {
-				return
-			}
-		}
-	}
+    return func(yield func(T, T) bool) {
+        for i := 0; i < len(xs)-1; i++ {
+            a := xs[i]
+            b := xs[i+1]
+            if !yield(a, b) {
+                return
+            }
+        }
+    }
 }
--- traffic/traffic.go
+++ traffic/traffic.go
@@ -8,130 +8,131 @@ import (
 )
 
 const vehicle_speed = 0.1
+const min_delta_perc = 0.05
 
 type Vehicle struct {
-	Id       int
-	Speed    float64
-	Target   *node.Node
-	At       *node.Node
-	Progress float64
-	Route    []*node.Node
+    Type     node.VehicleType
+    Id       int
+    Speed    float64
+    Target   *node.Node
+    At       *node.Node
+    Progress float64
+    Route    []*node.Node
 }
 
 type TrafficManager struct {
-	vehicle_next_id int
-	Graph           *node.Graph
-	Vehicles        []*Vehicle
+    vehicle_next_id int
+    Graph           *node.Graph
+    Pathfinder      node.Pathfinder
+    Vehicles        []*Vehicle
 }
 
 func NewTrafficManager(graph *node.Graph) TrafficManager {
-	return TrafficManager{
-		vehicle_next_id: 0,
-		Graph:           graph,
-		Vehicles:        []*Vehicle{},
-	}
+    return TrafficManager{
+        vehicle_next_id: 0,
+        Graph:           graph,
+        Pathfinder:      node.NewPathfinder(graph),
+        Vehicles:        []*Vehicle{},
+    }
 }
 
 func (t *TrafficManager) NewRandomVehicle() {
-	i := int(rand.Float64() * float64(len(t.Graph.Nodes)))
-	t.Vehicles = append(t.Vehicles, &Vehicle{
-		Id:       t.vehicle_next_id,
-		Speed:    vehicle_speed,
-		Target:   nil,
-		At:       t.Graph.Nodes[i],
-		Progress: 0,
-		Route:    nil,
-	})
+    i := int(rand.Float64() * float64(len(t.Graph.Nodes)))
+    t.Vehicles = append(t.Vehicles, &Vehicle{
+        Id:       t.vehicle_next_id,
+        Speed:    vehicle_speed,
+        Target:   nil,
+        At:       t.Graph.Nodes[i],
+        Progress: 0,
+        Route:    nil,
+    })
 
-	t.vehicle_next_id += 1
+    t.vehicle_next_id += 1
 }
 
 func (t *TrafficManager) Repath(v *Vehicle) error {
-	pf := node.NewPathfinder(t.Graph)
-	route, err := pf.Path(v.At, v.Target)
-	if err != nil {
-		return err
-	}
+    pf := &t.Pathfinder
+    newRoute, err := pf.Path(v.At, v.Target, v.Type)
+    if err != nil {
+        return err
+    }
 
-	v.Route = route[1:]
-
-	return nil
+    delta := 1 - (node.PathWeightedLength(v.Route, v.Type) / node.PathWeightedLength(newRoute, v.Type))
+    if delta > min_delta_perc {
+        v.Route = newRoute[1:]
+    }
+    return nil
 }
 
 func (v *Vehicle) AtTarget() bool {
-	return v.Target == v.At
+    return v.Target == v.At
 }
 
 func (t *TrafficManager) RandomTarget(v *Vehicle) {
-	i := int(rand.Float64() * float64(len(t.Graph.Nodes)))
-	v.Target = t.Graph.Nodes[i]
+    i := int(rand.Float64() * float64(len(t.Graph.Nodes)))
+    v.Target = t.Graph.Nodes[i]
 }
 
 func (v *Vehicle) InterpolatePos() node.Pos {
-	if len(v.Route) == 0 {
-		return v.At.Pos
-	}
+    if len(v.Route) == 0 {
+        return v.At.Pos
+    }
 
-	diff := v.At.Pos.Diff(v.Route[0].Pos)
-	diff.X *= v.Progress
-	diff.Y *= v.Progress
-	return diff
+    diff := v.At.Pos.Diff(v.Route[0].Pos)
+    diff.X *= v.Progress
+    diff.Y *= v.Progress
+    return diff
 }
 
 func (t *TrafficManager) VehiclesAsPoints() geojson.FeatureCollection[geojson.FlatGeometry] {
-	res := geojson.FeatureCollection[geojson.FlatGeometry]{}
-	for _, v := range t.Vehicles {
-		pos := v.InterpolatePos()
-		res = append(res, geojson.Feature[geojson.FlatGeometry]{
-			Geometry: geojson.FlatGeometry{
-				GeometryType: geojson.PointT,
-				SingleCoords: [2]float64{pos.X, pos.Y},
-			},
-			Props: map[string]any{},
-		})
-	}
-	return res
+    res := geojson.FeatureCollection[geojson.FlatGeometry]{}
+    for _, v := range t.Vehicles {
+        pos := v.InterpolatePos()
+        res = append(res, geojson.Feature[geojson.FlatGeometry]{
+            Geometry: geojson.FlatGeometry{
+                GeometryType: geojson.PointT,
+                SingleCoords: [2]float64{pos.X, pos.Y},
+            },
+            Props: map[string]any{},
+        })
+    }
+    return res
 }
 
+// Updates the state of all vehicles on the graph
 func (t *TrafficManager) Update(dt float64) {
-	for _, v := range t.Vehicles {
-		// assign new random target to vehicle if arrived at target
-		if v.AtTarget() || v.Route == nil {
-			if v.Target != nil {
-				log.Printf("vehicle: %d arrived at target: %d", v.Id, v.Target.Id)
-			}
-			t.RandomTarget(v)
-			v.Progress = 0
-			// create path based on situation
-			pf := node.NewPathfinder(t.Graph)
-			route, err := pf.Path(v.At, v.Target)
-			if err != nil {
-				log.Printf("vehicle: %d cant path to target: %s", v.Id, err)
-			} else {
-				v.Route = route[1:]
-			}
-		}
+    for _, v := range t.Vehicles {
+        // assign new random target to vehicle if arrived at target
+        if v.AtTarget() || v.Route == nil {
+            if v.Target != nil {
+                log.Printf("vehicle: %d arrived at target: %d", v.Id, v.Target.Id)
+            }
+            t.RandomTarget(v)
+            v.Progress = 0
+            // create path based on situation
+            err := t.Repath(v)
+            if err != nil {
+                log.Printf("vehicle: %d cant path to target: %s", v.Id, err)
+            }
+        }
 
-		// update vehicle progress
-		if len(v.Route) == 1 {
-			dist := v.At.Pos.DistanceTo(v.Target.Pos)
-			v.Progress += v.Speed * dt / dist
-			if v.Progress >= 1.0 {
-				v.At = v.Target
-			}
-		} else if len(v.Route) > 0 {
-			dist := v.At.Pos.DistanceTo(v.Route[0].Pos)
-			v.Progress += v.Speed * dt / dist
-			if v.Progress >= 1.0 {
-				v.At = v.Route[0]
-				pf := node.NewPathfinder(t.Graph)
-				new_route, err := pf.Path(v.At, v.Target)
-				if err != nil {
-					log.Printf("vehicle: %d cant path to target: %s", v.Id, err)
-				} else {
-					v.Route = new_route[1:]
-				}
-			}
-		}
-	}
+        // update vehicle progress
+        if len(v.Route) == 1 {
+            dist := v.At.Pos.DistanceTo(v.Target.Pos)
+            v.Progress += v.Speed * dt / dist
+            if v.Progress >= 1.0 {
+                v.At = v.Target
+            }
+        } else if len(v.Route) > 0 {
+            dist := v.At.Pos.DistanceTo(v.Route[0].Pos)
+            v.Progress += v.Speed * dt / dist
+            if v.Progress >= 1.0 {
+                v.At = v.Route[0]
+                err := t.Repath(v)
+                if err != nil {
+                    log.Printf("vehicle: %d cant path to target: %s", v.Id, err)
+                }
+            }
+        }
+    }
 }
