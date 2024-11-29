d6afaeb2957b0f46884d24d241ae0435f553a4e8
CoinBench2582 <coinbench.riot@outlook.com>
2024-11-29 10:02:10 +0100

    jednotkovy vektor

--- dashboard/index.js
+++ dashboard/index.js
@@ -88,8 +88,10 @@ const magicShift = 0.05
 const interpolPos = (coordsSource, coordsDest) => {
     let horizontalDis = coordsSource[0] - coordsDest[0]
     let verticalDis = coordsSource[1] - coordsDest[1]
-    let dalka = Math.sqrt(horizontalDis*horizontalDis + verticalDis*verticalDis)
-    return [coordsDest[0] + magicShift*horizontalDis/dalka, coordsDest[1] + magicShift*verticalDis/dalka]
+    let dalka = Math.sqrt(horizontalDis * horizontalDis + verticalDis * verticalDis)
+    let newHoriz = horizontalDis / dalka
+    let newVertic = verticalDis / dalka
+    return [coordsDest[0] + newHoriz*magicShift, coordsDest[1] + magicShift*newVertic]
 }
 
 const split_lights = (fc) => {
